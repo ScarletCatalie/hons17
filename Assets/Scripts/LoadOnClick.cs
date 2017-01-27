83b880559d615e5dec576ab4b83b8e6b128f0687
ScarletCatalie <claytonNatalie94@outlook.com>
2017-01-27 10:19:48 +0000

    Main Menu, Scene Transition.
    Signed-off-by: ScarletCatalie <claytonNatalie94@outlook.com>

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+       public void LoadScene(int level)
+    {
+        SceneManager.LoadScene("Intro_Station");
+    }
+
+
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public GameObject PlayButton;
+
+
+	public void OnStartGame () {
+        SceneManager.LoadScene("Int_Station");
+	}
+	
+
+}
