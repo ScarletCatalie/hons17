6e4a5789639a10e0f9d414f5b5e107dcda8574c1
ScarletCatalie <claytonNatalie94@outlook.com>
2017-01-31 14:10:47 +0000

    minor changes to station

---
+++ Assets/GenericTrigger/GenericTrigger/Editor/GenericTriggerOverride.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+[CustomEditor(typeof(GenericTrigger))]
+public class GenericTriggerOverride : Editor {
+
+    GenericTrigger gt;
+
+    public override void OnInspectorGUI()
+    {
+        //target = base.target(GenericTrigger);
+        //base.OnInspectorGUI();
+
+        gt = (GenericTrigger)target;
+
+        gt.TypeOfTrigger = (GenericTrigger.TrigType)EditorGUILayout.EnumPopup("Type of Trigger: ",gt.TypeOfTrigger);
+
+        if(gt.TypeOfTrigger == GenericTrigger.TrigType.OnClick)
+        {
+            gt.mouseButton = EditorGUILayout.IntField("Mouse Button Number: ", gt.mouseButton);
+        }
+
+        gt.sendGameObjectReference = EditorGUILayout.Toggle("Send GameObject: ", gt.sendGameObjectReference);
+
+        gt.go =(GameObject)EditorGUILayout.ObjectField("GameObject: ", gt.go,typeof(GameObject),true);
+
+        if(gt.go != null)
+        {
+            if (mons.Count < 1)
+            {
+                refreshScripts();
+
+            }
+            else
+            {
+                selection = EditorGUILayout.Popup("Script: ",selection, nms.ToArray());
+                gt.so = mons[selection].GetType();
+
+                //if(funcsList.Count < 1)
+                //{
+                //    //refreshFuncs();
+
+                //}
+                //else
+                //{
+                //selectionFunc = EditorGUILayout.TextArea( selectionFunc); //EditorGUILayout.Popup("Function: ", selection, funcsList.ToArray());
+                //gt.function = funcsList[selectionFunc];
+                gt.function = EditorGUILayout.TextField("Function name", gt.function);
+                //}
+            }
+
+            
+        }
+        
+        if(GUILayout.Button("Refresh Scripts"))
+        {
+            refreshScripts();
+        }
+        //if(GUILayout.Button("Refresh Functions"))
+        //{
+        //    refreshFuncs();
+        //}
+
+
+    }
+    int selection = 0;
+    int selectionFunc = 0;
+    List<MonoBehaviour> mons = new List<MonoBehaviour>();
+    List<string> nms = new List<string>();
+
+    List<string> funcsList = new List<string>();
+
+    void refreshScripts()
+    {
+        selection = 0;
+        mons = new List<MonoBehaviour>( gt.go.GetComponents<MonoBehaviour>());
+
+        nms = new List<string>();
+        foreach (MonoBehaviour mb in mons)
+        {
+            nms.Add(mb.ToString());
+        }
+
+    }
+    //void refreshFuncs()
+    //{
+    //    funcsList = new List<string>();
+
+    //    MemberInfo[] memberInfos = mons[selection].GetType().GetMembers(BindingFlags.Public | BindingFlags.Instance);
+    //    Debug.Log(memberInfos.Length);
+    //    foreach (MemberInfo mi in memberInfos)
+    //    {
+    //        funcsList.Add(mi.ToString());
+    //    }
+
+    //}
+    
+}
---
+++ Assets/GenericTrigger/GenericTrigger/GenericTrigger.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class GenericTrigger : MonoBehaviour {
+
+    public enum TrigType { None,CollisionEnter,CollisionExit,TriggerEnter,TriggerExit,OnClick}
+
+    public TrigType TypeOfTrigger = TrigType.None;
+
+    public int mouseButton = 0;
+
+    public bool sendGameObjectReference;
+
+    //[System.Serializable]
+    public GameObject go;
+
+    //[System.Serializable]
+    public Type so;
+
+    //[SerializeField]
+    public string function;
+
+    //TRIGGER STUFF
+    void Trig()
+    {
+        MonoBehaviour mb;
+        foreach (Component co in go.GetComponents<MonoBehaviour>())
+        {
+            if(co.GetType() == so)
+            {
+                mb = (MonoBehaviour)co;
+                if (sendGameObjectReference)
+                {
+                    mb.SendMessage(function,this.gameObject);
+                }
+                else
+                {
+                    mb.SendMessage(function);
+                }
+                return;
+            }
+        }
+
+        //MethodInfo meth = mb.GetMethod(function);
+
+        //(MonoBehaviour)go.GetComponent(so).invok
+
+    }
+
+    void check()
+    {
+        //Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+
+        MemberInfo[] memberInfos = so.GetMembers(BindingFlags.Public);
+
+        Component[] allComponents;
+
+        allComponents = go.GetComponents<MonoBehaviour>();
+
+        //memberInfos[0].Name;
+
+        //foreach (Type toCheck in so)
+        //{
+        //    MemberInfo[] memberInfos = toCheck.GetMembers(BindingFlags.Public);
+        //    //Loop over the membersInfos and do what you need to such as retrieving their names
+        //    // and adding them to a file
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (TypeOfTrigger == TrigType.TriggerEnter)
+        {
+            Trig();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (TypeOfTrigger == TrigType.TriggerExit)
+        {
+            Trig();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (TypeOfTrigger == TrigType.CollisionEnter)
+        {
+            Trig();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (TypeOfTrigger == TrigType.CollisionExit)
+        {
+            Trig();
+        }
+    }
+
+    bool mouseDowned = false;
+
+	// Update is called once per frame
+	void Update () {
+		
+        if(TypeOfTrigger == TrigType.OnClick)
+        {
+            if(Input.GetMouseButtonDown(mouseButton))
+            {
+                Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                RaycastHit hitInfo = new RaycastHit();
+
+                Physics.Raycast(r, out hitInfo);
+
+                if(hitInfo.collider!= null && hitInfo.collider.gameObject == this.gameObject)
+                {
+                    mouseDowned = true;
+                }
+            }
+            else if(mouseDowned && Input.GetMouseButtonUp(mouseButton))
+            {
+                mouseDowned = false;
+
+                Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                RaycastHit hitInfo = new RaycastHit();
+
+                Physics.Raycast(r, out hitInfo);
+
+                if (hitInfo.collider != null && hitInfo.collider.gameObject == this.gameObject)
+                {
+                    Trig();
+                }
+            }
+        }
+
+	}
+}
